3eac87bfa005c9ff9d08acd677ddfe7f145c33b7
Joshua Comeau <joshwcomeau@gmail.com>
2015-03-22 11:31:25 -0400

    Added a 'recent move state' property to GameStore, so that now we know exactly what the last move was.

--- public/js/stores/GameStore.js
+++ public/js/stores/GameStore.js
@@ -17,6 +17,16 @@ var _board  = [];
 var _active = []; // Holds currently-activated cells
 var _currentWord = "";
 
+var _moveStates = {
+  WORD_ACCEPTED: "WORD_ACCEPTED",
+  WORD_REJECTED: "WORD_REJECTED",
+  TILE_ACTIVATED: "TILE_ACTIVATED",
+  TILE_DEACTIVATED: "TILE_DEACTIVATED",  // A single tile deactivated; you undid last move.
+  WORD_DEACTIVATED: "WORD_DEACTIVATED",  // you clicked a tile in your word and deactivated it.
+  NEW_WORD_STARTED: "NEW_WORD_STARTED"   // you clicked a non-neighboring cell and started a new word.
+}
+var _recentMoveState = null;
+
 
 function resetBoard(col, row) {
   var column, letters, letter;
@@ -60,8 +70,11 @@ function clickTile(column, row) {
     // If we're in the process of building a word, we're only allowed to deactivate
     // the last letter. If we click on any other active tile, we reset the board.
     if ( clickedTile !== _active[_active.length-1] ) {
+      _recentMoveState = _moveStates.WORD_DEACTIVATED;
       setAllToInactive();
     } else {
+      _recentMoveState = _moveStates.TILE_DEACTIVATED;
+
       var newTile = {
         letter: clickedTile.letter,
         active: false
@@ -82,7 +95,10 @@ function clickTile(column, row) {
     // The tile we just clicked is not active. We need to activate it,
     // but we also need to DEactivate any active tiles that aren't neighboring this one.
     if ( _active.length && _.intersection(neighbors, _active).length === 0 ) {
+      _recentMoveState = _moveStates.NEW_WORD_STARTED;
       setAllToInactive();
+    } else {
+      _recentMoveState = _moveStates.TILE_ACTIVATED;
     }
 
     var newTile = {
@@ -117,7 +133,11 @@ function evaluateWord() {
         }
       });
     });
-  } 
+
+    _recentMoveState = _moveStates.WORD_ACCEPTED;
+  } else {
+    _recentMoveState = _moveStates.WORD_REJECTED;
+  }
 
   // Whether the word was valid or not, we need to deactivate all the letters.
   setAllToInactive();
@@ -130,6 +150,8 @@ var GameStore = _.extend({}, EventEmitter.prototype, {
   getTime:  function() { return _time;        },
   getBoard: function() { return _board;       },
   getWord:  function() { return _currentWord; },
+
+  getRecentMove: function() { return _recentMoveState; },
 
   isActiveTile: function(column, row) {
     return _board[column][row] ? _board[column][row].active : false;
