091debd856c7c5647a79f23e9f9022b55578ca37
Joshua Comeau <joshwcomeau@gmail.com>
2015-03-25 12:44:25 -0400

    got a messy but working solution for inserting the current user score in the high scores feed from Firebase.

--- public/js/components/HighScores.jsx
+++ public/js/components/HighScores.jsx
@@ -9,6 +9,10 @@ function getState() {
   };
 }
 
+function formatScores(scores) {
+
+}
+
 module.exports = React.createClass({
 
   mixins: [ReactFireMixin],
@@ -27,18 +31,52 @@ module.exports = React.createClass({
     //   });
     // }.bind(this));
   },
-
   render: function() {
-    console.log("HighScore rendered")
-    console.log(this.state);
-    var highScoreNodes = this.state.scores.map(function(score, index) {
-      return (
-        <div className="high-score-row">
-          <span className="rank">{index+1}</span>
-          <span className="name">{score.name}</span>
-          <span className="score">{score.score}</span>
-        </div>
-      );
+    // We need to iterate through our scores and, if our score is higher than one of them, 
+    // 'insert' the custom user score holder. Bit messy, but it'll work for a first-pass.
+
+    var currentScore    = GameStore.getScore(); 
+    var scoreArray      = this.state.scores.slice(0).sort(function(a, b) { 
+      return a.score >= b.score ? 1 : -1
+    });
+
+    if ( scoreArray.length && currentScore > scoreArray[0].score ) {
+      var newScoreIndex;
+
+      scoreArray.forEach(function(scoreItem, index) {
+        if ( scoreItem.score > currentScore && !newScoreIndex) {
+          newScoreIndex = index;
+        }
+      });
+
+      scoreArray.splice(newScoreIndex, 0, {
+        type: 'current_user_score',
+        name: '',
+        score: currentScore
+      });
+
+    }
+
+
+    var highScoreNodes  = scoreArray.map(function(scoreItem, index) {
+      if ( scoreItem.type === 'current_user_score' ) {
+        return (
+          <div className="high-score-row" key={index}>
+            <span className="rank">{index+1}</span>
+            <span className="name"><input type="text" placeholder="Enter Name_" /></span>
+            <span className="score">{scoreItem.score}</span>
+          </div>
+        );
+      } else {
+        return (
+          <div className="high-score-row" key={index}>
+            <span className="rank">{index+1}</span>
+            <span className="name">{scoreItem.name}</span>
+            <span className="score">{scoreItem.score}</span>
+          </div>
+        );  
+      }
+      
     });
 
     return (
