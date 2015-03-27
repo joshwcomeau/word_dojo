7c16fb5ab6624007eebcdf901b648b8817057357
Joshua Comeau <joshwcomeau@gmail.com>
2015-03-27 08:58:16 -0400

    A few more subtle refactors to HighScores component.

--- public/js/components/HighScores.jsx
+++ public/js/components/HighScores.jsx
@@ -17,6 +17,7 @@ function formatScores(scores) {
 
 
 var interval;
+var NUM_OF_HIGH_SCORES = 8;
 
 module.exports = React.createClass({
 
@@ -72,19 +73,18 @@ module.exports = React.createClass({
     };
   },
 
+  getSortedHighScores: function() {
+    return this.state.scores.sort(function(a, b) { 
+      return a.score < b.score ? 1 : -1;
+    }).slice(0, NUM_OF_HIGH_SCORES);
+  },
+
   buildScoresArray: function() {
     var currentScore, scoreArray, newScoreIndex, newScore, lowestScore;
-    var numOfHighScores = 8;
     
     currentScore  = GameStore.getScore(); 
-
-    // Sort it from highest to lowest, keep the first 10
-    scoreArray = this.state.scores.sort(function(a, b) { 
-      return a.score < b.score ? 1 : -1;
-    }).slice(0, numOfHighScores);
-
-
-    newScore = this.buildNewScoreObject(currentScore);
+    scoreArray    = this.getSortedHighScores();
+    newScore      = this.buildNewScoreObject(currentScore);
 
     // If we've already submitted a score (this is a re-render), or we haven't solved any words (no '0' high scores)
     if (this.state.scoreSubmitted || currentScore === 0) return scoreArray;
@@ -99,7 +99,7 @@ module.exports = React.createClass({
 
     // If there are empty spaces on the high score board, OR if our score is highest than the lowest score,
     // we need to add our newScore object somewhere in the array.
-    if ( currentScore > lowestScore || scoreArray.length < numOfHighScores ) {
+    if ( currentScore > lowestScore || scoreArray.length < NUM_OF_HIGH_SCORES ) {
       
       // Find the index of the first score less than the user's
       newScoreIndex = _.findIndex(scoreArray, function(scoreItem) {
@@ -110,11 +110,7 @@ module.exports = React.createClass({
         // Our new score is somewhere in-between or on top of the other scores. Add it in the appropriate place.
         scoreArray.splice(newScoreIndex, 0, newScore);
         // Remove the lowest score; we've bumped it from the equation
-        console.log(scoreArray[scoreArray.length-1])
         scoreArray.splice(-1, 1);
-        console.log(scoreArray[scoreArray.length-1])
-
-
       } else {
         // This is the rare edge case when there are less than 10 scores on the board, 
         // and we're lower than all of them.
