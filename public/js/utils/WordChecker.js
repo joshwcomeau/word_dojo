b092fc68dbd6ec570ebc75c073dfafbd0dcd9dd5
Joshua Comeau <joshwcomeau@gmail.com>
2015-03-31 05:59:26 -0400

    Refactored WordChecker utility. Now, instead of parsing through one massive array, it parses through a smaller array that only holds the words of equal length. A word with 3 letters only checks an array with other 3-letter words.

--- public/js/utils/WordChecker.js
+++ public/js/utils/WordChecker.js
@@ -2,15 +2,42 @@
 // Validates whether a word is really a word.
 //
 
-var words = [];
+var words = {};
 
-$.get('words.txt').done(function(text) {
-  words = text.split(",");
-});
+// Returns an object with the key being the number of letters, the value being an array of
+// words with that many letters.
+// eg. {
+//   3: ['egg', 'aye', 'hey'...],
+//   4: ['hero', 'boom', 'fuck'...] 
+// }
+function buildWordObj(path) {
+  var word, wordLength, wordArray;
+
+  // Start by fetching the data and splitting it into an array
+  $.get(path).done(function(text) {
+    wordArray = text.split(",");
+
+    wordArray.forEach(function(word) {
+      wordLength = word.length;
+
+      // If there already is a key of that length
+      if ( words[wordLength] ) {
+        words[wordLength].push(word);
+      }
+      // This is the first word of this length
+      else {
+        words[wordLength] = [word];
+      }
+    });
+  });
+}
+
+buildWordObj('words.txt');
 
 var WordChecker = {
   validateWord: function(word) {
-    return words.indexOf(word.toLowerCase()) !== -1;
+    console.log(words);
+    return words[word.length].indexOf(word.toLowerCase()) !== -1;
   }
 };
 
